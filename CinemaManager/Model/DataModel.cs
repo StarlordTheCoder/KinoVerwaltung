93759c6208caa71e3ddd71648dd51ecf2adf7d74
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2016-05-31 10:34:49 +0200

    Grundlage Serialize

---
+++ CinemaManager/Model/DataModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManager.Model
+{
+	[Serializable]
+	public class DataModel
+	{
+		[NonSerialized]
+		private readonly BinaryFormatter _binaryFormatter;
+
+		[NonSerialized]
+		private static string _path = "TODO";
+
+		public IEnumerable<CinemaModel> Cinemas { get; private set; }
+
+		public DataModel()
+		{
+			_binaryFormatter = new BinaryFormatter();
+		}
+
+		public void Save()
+		{
+			//TODO
+			_binaryFormatter.Serialize(File.OpenWrite(_path), this);
+		}
+
+		public void Load()
+		{
+			//TODO
+			var result = (DataModel) _binaryFormatter.Deserialize(File.OpenWrite(_path));
+			Cinemas = result.Cinemas;
+		}
+	}
+}
