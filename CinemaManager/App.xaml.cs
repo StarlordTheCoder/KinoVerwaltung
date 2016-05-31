4572db3a60c975ddaba00c98e3ec9fd0555b263e
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2016-05-31 10:25:58 +0200

    File Association und Module-Platzhalter.

--- CinemaManager/App.xaml.cs
+++ CinemaManager/App.xaml.cs
@@ -1,14 +1,28 @@
 ﻿// CinemaManager created by Seraphin, Pascal & Alain as a school project
 // Copyright (c) 2016 All Rights Reserved
 
+using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace CinemaManager
 {
 	/// <summary>
 	///     Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application
+	public partial class App
 	{
+		private void App_OnStartup(object sender, StartupEventArgs e)
+		{
+			var layoutToLoad = e.Args.FirstOrDefault(File.Exists);
+
+			new MainWindow(layoutToLoad).Show();
+		}
+
+		private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show(e.Exception.StackTrace);
+		}
 	}
 }
--- CinemaManager/MainWindowViewModel.cs
+++ CinemaManager/MainWindowViewModel.cs
@@ -1,8 +1,12 @@
 ﻿// CinemaManager created by Seraphin, Pascal & Alain as a school project
 // Copyright (c) 2016 All Rights Reserved
 
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 using CinemaManager.Modules;
 using CinemaManager.Modules.Cinema;
 using CinemaManager.Modules.User;
@@ -22,19 +26,47 @@ namespace CinemaManager
 			LoadLayoutCommand = new DelegateCommand(LoadLayout);
 			SaveLayoutCommand = new DelegateCommand(SaveLayout);
 
+			ChangeDataCommand = new DelegateCommand(ChangeData);
+
+			AboutCommand = new DelegateCommand(() => MessageBox.Show(AboutMessage));
+
 			_layoutSerializer = new XmlLayoutSerializer(dockManager);
+
+			_layoutSerializer.LayoutSerializationCallback += (sender, e) =>
+			{
+				var module = Modules.First(m => m.Title == e.Model.Title);
+
+				//TODO
+
+				e.Cancel = true;
+			};
 
 			Modules = new ObservableCollection<IModule> {CinemaModule, UserModule};
 		}
 
 		#region Modules
 
-		public IModule UserModule { get; } = new UserModule();
 		public IModule CinemaModule { get; } = new CinemaModule();
+		public IModule MovieModule { get; }
+		public IModule PresentationModule { get; }
+		public IModule ReservationModule { get; }
+		public IModule UserModule { get; } = new UserModule();
 
 		#endregion
 
 		#region Layout
+
+		public void LoadLayout(string path)
+		{
+			try
+			{
+				_layoutSerializer.Deserialize(path);
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Error loading layout!");
+			}
+		}
 
 		private void LoadLayout()
 		{
@@ -47,16 +79,7 @@ namespace CinemaManager
 			var result = dialog.ShowDialog();
 			if (result.HasValue && result.Value)
 			{
-				_layoutSerializer.LayoutSerializationCallback += (sender, e) =>
-				{
-					var module = Modules.First(m => m.Title == e.Model.Title);
-
-					//TODO
-
-					e.Cancel = true;
-				};
-
-				_layoutSerializer.Deserialize(dialog.FileName);
+				LoadLayout(dialog.FileName);
 			}
 		}
 
@@ -75,11 +98,41 @@ namespace CinemaManager
 			}
 		}
 
-		public DelegateCommand LoadLayoutCommand { get; }
-
-		public DelegateCommand SaveLayoutCommand { get; }
+		public ICommand LoadLayoutCommand { get; }
+		public ICommand SaveLayoutCommand { get; }
 
 		public ObservableCollection<IModule> Modules { get; }
+
+		#endregion
+
+		#region Data
+
+		private void ChangeData()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ICommand ChangeDataCommand { get; }
+
+		#endregion
+
+		#region About
+
+		public ICommand AboutCommand { get; }
+
+		private static readonly string AboutMessage =
+			"Created by Seraphin Rihm, Pascal Honegger & Alain Keller" + Environment.NewLine
+			+ "Version " + ApplicationVersion;
+
+		private static string ApplicationVersion
+		{
+			get
+			{
+				var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+				var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+				return fvi.FileVersion;
+			}
+		}
 
 		#endregion
 	}
