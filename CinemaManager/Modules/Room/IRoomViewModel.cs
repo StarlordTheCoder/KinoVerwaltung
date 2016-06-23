16c8aa55302c2aa551ce67d19fec2aa0983c8484
lordmcfungus <alain.keller@edu.tbz.ch>
2016-06-23 14:52:25 +0200

    added roomtest

---
+++ CinemaManager/Modules/Room/IRoomViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using CinemaManager.Model;
+
+namespace CinemaManager.Modules.Room
+{
+	public interface IRoomViewModel
+	{
+		RoomModel Model { get; set; }
+		ObservableCollection<RowViewModel> Rows { get; }
+		RowViewModel SelectedRow { get; set; }
+		ObservableCollection<SeatViewModel> SelectedSeats { get; }
+
+		void AddRow();
+		void AddSeat();
+		void RemoveRow();
+		void RemoveSeat();
+	}
+}
